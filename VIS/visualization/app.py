8e054b5a9b185e02a11e12f2cc2650723089aaee
Lynch1206 <94370749+Lynch1206@users.noreply.github.com>
2023-02-03 17:09:37 +0100

    min_comment

--- VIS/visualization/app.py
+++ VIS/visualization/app.py
@@ -42,11 +42,13 @@ construct_year_list = violin[violin['age'].notna()
 
 token = 'pk.eyJ1IjoiZ29sZGVkaXRpb24yMTIiLCJhIjoiY2tld3dvMGxmMGJsbjM1bXV5cXNjam84cSJ9.32Xt4hp12-2Fa3Rk2XFLgQ'
 airbnb = pd.read_csv('preprocessed.csv')
+#feature selection and data normalization
 features = ["nei_price", "age", "availability_365",'lat','long']
 min_max_scalar = preprocessing.MinMaxScaler(feature_range=(0,1))
 map_data = airbnb[features]
 map_data['price_distribution'] = min_max_scalar.fit_transform(map_data['nei_price'].values.reshape(-1,1))
 map_data['age_distribution'] = min_max_scalar.fit_transform(map_data['age'].values.reshape(-1,1))
+#compute the mean and standard variance and filter the data in terms of mean and variance
 mean_ava = map_data['availability_365'].mean()
 sd = map_data['availability_365'].std()
 map_data = map_data[(map_data['availability_365'] <= mean_ava+(3*sd))]
@@ -156,12 +158,11 @@ app.layout = html.Div([
     Output("map", "figure"),
     Input("candidate", "value"))
 def display_choropleth(candidate):
-    df = map_data  # replace with your own data source
+    df = map_data  
     fig = px.scatter_mapbox(df, lat='lat', lon='long', color=candidate,
                             range_color=[0, 1],
                             color_continuous_scale=px.colors.cyclical.IceFire
                             )
-    # fig.update_layout(mapbox_style="dark", mapbox_accesstoken=token)
     fig.update_layout(mapbox_style="carto-positron")
     fig.update_layout(margin=dict(b=0, t=0, l=0, r=0))
 
--- VIS/visualization/preprocess.py
+++ VIS/visualization/preprocess.py
@@ -2,7 +2,9 @@ import pandas as pd
 import numpy as np
 
 airbnb = pd.read_csv('airbnb_open_data.csv')
+#replace the space by the _ for all column
 airbnb.columns=[col.lower().replace(" ","_") for col in airbnb.columns]
+# remove the dallar sign
 def remove_dollar_sign(value):
     if pd.isna(value):
         return np.NaN
@@ -11,6 +13,9 @@ def remove_dollar_sign(value):
 
 airbnb["price"]=airbnb["price"].apply(lambda x: remove_dollar_sign(x))
 airbnb["service_fee"]=airbnb["service_fee"].apply(lambda x: remove_dollar_sign(x))
+
+#compute the mean price of each neighbourhood
 airbnb['nei_price'] = airbnb['price'].groupby(airbnb['neighbourhood']).transform('mean')
+#compute the age of properties
 airbnb['age'] = 2023 - airbnb['construction_year']
 airbnb.to_csv('preprocessed.csv')
